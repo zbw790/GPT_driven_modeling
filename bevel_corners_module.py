c31516837e8f8b8103acd7611514e1399a70962d
zbw790 <101329662+BowenZhu790@users.noreply.github.com>
2024-07-21 15:42:40 +0100

    default bevel operation module

--- bevel_corners_module.py
+++ bevel_corners_module.py
@@ -1,13 +1,12 @@
 import bpy
-from bpy.types import Operator, Panel
-from bpy.props import FloatProperty, IntProperty, EnumProperty, BoolProperty
+from bpy.types import Operator
 
 class BevelEdgesOperator(Operator):
     bl_idname = "object.bevel_edges"
     bl_label = "Bevel Edges"
     bl_options = {'REGISTER', 'UNDO'}
 
-    width: FloatProperty(
+    width: bpy.props.FloatProperty(
         name="Width",
         description="Bevel width",
         default=0.1,
@@ -15,76 +14,12 @@ class BevelEdgesOperator(Operator):
         max=1.0
     )
 
-    segments: IntProperty(
+    segments: bpy.props.IntProperty(
         name="Segments",
         description="Number of segments for bevel",
         default=4,
         min=1,
-        max=100
-    )
-
-    angle_limit: FloatProperty(
-        name="Angle Limit",
-        description="Angle above which to bevel edges",
-        default=30.0,
-        min=0.0,
-        max=180.0,
-        subtype='ANGLE'
-    )
-
-    offset_type: EnumProperty(
-        name="Offset Type",
-        description="How to measure the width of the bevel",
-        items=[
-            ('OFFSET', "Offset", "Amount is offset of new edges from original"),
-            ('WIDTH', "Width", "Amount is width of new face"),
-            ('DEPTH', "Depth", "Amount is perpendicular distance from original edge to bevel face"),
-            ('PERCENT', "Percent", "Amount is percent of adjacent edge length"),
-        ],
-        default='OFFSET'
-    )
-
-    profile: FloatProperty(
-        name="Profile",
-        description="The profile shape (0.5 = round)",
-        default=0.5,
-        min=0.0,
-        max=1.0
-    )
-
-    miter_outer: EnumProperty(
-        name="Outer Miter",
-        description="Pattern to use for outside of miters",
-        items=[
-            ('SHARP', "Sharp", "Sharp corners"),
-            ('PATCH', "Patch", "Create a patch to smooth corners"),
-            ('ARC', "Arc", "Create an arc to smooth corners"),
-        ],
-        default='SHARP'
-    )
-
-    miter_inner: EnumProperty(
-        name="Inner Miter",
-        description="Pattern to use for inside of miters",
-        items=[
-            ('SHARP', "Sharp", "Sharp corners"),
-            ('ARC', "Arc", "Create an arc to smooth corners"),
-        ],
-        default='SHARP'
-    )
-
-    spread: FloatProperty(
-        name="Spread",
-        description="Amount to spread beveled vertices",
-        default=0.0,
-        min=0.0,
-        max=1.0
-    )
-
-    harden_normals: BoolProperty(
-        name="Harden Normals",
-        description="Match normals of new faces to adjacent faces",
-        default=False
+        max=20
     )
 
     def execute(self, context):
@@ -94,20 +29,13 @@ class BevelEdgesOperator(Operator):
                 bevel_modifier.width = self.width
                 bevel_modifier.segments = self.segments
                 bevel_modifier.limit_method = 'ANGLE'
-                bevel_modifier.angle_limit = self.angle_limit
-                bevel_modifier.offset_type = self.offset_type
-                bevel_modifier.profile = self.profile
-                bevel_modifier.miter_outer = self.miter_outer
-                bevel_modifier.miter_inner = self.miter_inner
-                bevel_modifier.spread = self.spread
-                bevel_modifier.harden_normals = self.harden_normals
-                
+                bevel_modifier.angle_limit = 1.22173  # 约70度
                 bpy.context.view_layer.objects.active = obj
                 bpy.ops.object.modifier_apply(modifier="Bevel")
         return {'FINISHED'}
 
-class OBJECT_PT_bevel_panel(Panel):
-    bl_label = "Advanced Bevel Edges"
+class OBJECT_PT_bevel_panel(bpy.types.Panel):
+    bl_label = "Bevel Edges"
     bl_idname = "OBJECT_PT_bevel_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -115,15 +43,4 @@ class OBJECT_PT_bevel_panel(Panel):
 
     def draw(self, context):
         layout = self.layout
-        
-        op = layout.operator("object.bevel_edges")
-        
-        layout.prop(op, "width")
-        layout.prop(op, "segments")
-        layout.prop(op, "angle_limit")
-        layout.prop(op, "offset_type")
-        layout.prop(op, "profile")
-        layout.prop(op, "miter_outer")
-        layout.prop(op, "miter_inner")
-        layout.prop(op, "spread")
-        layout.prop(op, "harden_normals")
+        layout.operator("object.bevel_edges")
